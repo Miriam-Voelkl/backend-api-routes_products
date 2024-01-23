8da4d421de3d689f708fa25ae81f3daa81604bec
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-23 16:03:59 +0100

    add error handling

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -3,5 +3,8 @@ import { getProductById } from "@/services/productServices";
 export default function handler(request, response) {
   const { id } = request.query;
   const currentProduct = getProductById(id);
+  if (!currentProduct) {
+    response.status(404).json({ status: "not found" });
+  }
   response.status(200).json(currentProduct);
 }
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -2,5 +2,8 @@ import { getAllProducts } from "@/services/productServices";
 
 export default function handler(request, response) {
   const allProducts = getAllProducts();
+  if (!allProducts) {
+    response.status(404).json({ status: "not found" });
+  }
   response.status(200).json(allProducts);
 }
